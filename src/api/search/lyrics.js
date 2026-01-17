1a591afbdc063d140e64b834b0f13f2e00c41678
7eppeli <baniriry@gmail.com>
2026-01-17 18:31:31 +0700

    Zzz

---
+++ src/api/search/lyrics.js
@@ -0,0 +1,57 @@
+const axios = require("axios")
+const cheerio = require("cheerio")
+ 
+async function lirik(search) {
+  try {
+    let { data } = await axios.get(`https://www.lyrics.com/lyrics/${search}`, {
+      headers: {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36',
+        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
+        'Accept-Encoding': 'gzip, deflate',
+        'Accept-Language': 'en-US,en;q=0.9',
+        'Connection': 'keep-alive',
+        'Cache-Control': 'no-cache',
+        'DNT': '1',
+        'Upgrade-Insecure-Requests': '1'
+      }
+    });
+ 
+    let $ = cheerio.load(data);
+    let results = [];
+ 
+    $('.sec-lyric').each((i, el) => {
+      let title = $(el).find('.lyric-meta-title a').text().trim();
+      let artist = $(el).find('.lyric-meta-album-artist a, .lyric-meta-artists a').text().trim();
+      let lyric = $(el).find('.lyric-body').text().trim();
+ 
+      if (title && artist && lyric) {
+        results.push({ title, artist, lyricSingkat: lyric });
+      }
+    });
+ 
+    return results;
+  } catch (error) {
+    console.error('Error fetching lyrics:', error);
+    return [];
+  }
+}
+module.exports = function(app) {
+    app.get('/sr/lyrics', async (req, res) => {
+        const { query } = req.query;
+        if (!query) {
+            return res.status(400).json({
+              status: false,
+              error: '"query" is required'
+            });
+        }
+        try {
+            const result = await lirik(query);
+            res.status(200).json({
+                status: true,
+                result
+            });
+        } catch (error) {
+            res.status(500).json({ status: false, error: error.message });
+        }
+    });
+}
